370588cf73c268eb5eeadaa45095930bd6fd1b7a
vivek-quantbit <vivek.kumbhar@erpdata.in>
2026-03-18 16:37:02 +0530

    report

--- library_management/library_management/report/project_timeline/project_timeline.js
+++ library_management/library_management/report/project_timeline/project_timeline.js
@@ -1,23 +1,11 @@
-// Copyright (c) 2026, Vaibhav and contributors
-// For license information, please see license.txt
-
-// frappe.query_reports["Project Timeline"] = {
-// 	"filters": [
-
-// 	]
-// };
-
 frappe.query_reports["Project Timeline"] = {
 	filters: [
 		{
-			fieldname: "from_date",
-			label: "From Date",
-			fieldtype: "Date"
-		},
-		{
-			fieldname: "to_date",
-			label: "To Date",
-			fieldtype: "Date"
+			fieldname: "view_by",
+			label: "View By",
+			fieldtype: "Select",
+			options: "Customer\nUser",
+			default: "Customer"
 		},
 		{
 			fieldname: "customer",
--- library_management/library_management/report/project_timeline/project_timeline.py
+++ library_management/library_management/report/project_timeline/project_timeline.py
@@ -8,10 +8,6 @@
 # 	columns, data = [], []
 # 	return columns, data
 
-
-# Copyright (c) 2026
-# For license information, please see license.txt
-
 # Copyright (c) 2026, Vaibhav and contributors
 # For license information, please see license.txt
 
@@ -32,7 +28,7 @@ def execute(filters=None):
     schedule_entries, all_dates = build_schedule(tickets)
 
     columns = get_columns(all_dates)
-    data = get_data(schedule_entries, all_dates)
+    data = get_data(schedule_entries, all_dates, filters)
 
     return columns, data
 
@@ -49,19 +45,12 @@ def get_tickets(filters):
         conditions.append("customer = %(customer)s")
         values["customer"] = filters.get("customer")
 
-    if filters.get("from_date"):
-        conditions.append("date(creation) >= %(from_date)s")
-        values["from_date"] = filters.get("from_date")
-
-    if filters.get("to_date"):
-        conditions.append("date(creation) <= %(to_date)s")
-        values["to_date"] = filters.get("to_date")
-
     sql = f"""
         SELECT
             name,
             customer,
             creation,
+            status,
             _assign,
             custom_expected_working_hours
         FROM `tabHD Ticket`
@@ -84,6 +73,7 @@ def get_tickets(filters):
             "ticket": row.name,
             "customer": row.customer or "Not Set",
             "creation": row.creation,
+            "status": row.status or "",
             "hours": float(row.custom_expected_working_hours or 0),
             "assigned_users": assigned_users,
         })
@@ -107,13 +97,12 @@ def parse_assign(assign_value):
 
 def build_schedule(tickets):
     """
-    Final business rules:
-    1. Sort tickets by creation ascending
-    2. For multi-user assignment, EACH assigned user gets full task hours
-    3. Each user has own queue
-    4. User must finish current task before moving to next
-    5. After finishing a task, remaining same-day hours can be used for next task
-    6. Task cannot start before its creation datetime
+    Rules:
+    1. Plan from today onward
+    2. Status 'Working' gets highest priority
+    3. After that, priority by creation ASC
+    4. If multiple users assigned, each user gets full task hours separately
+    5. User finishes current task first, then next
     """
 
     user_queues = defaultdict(list)
@@ -124,6 +113,7 @@ def build_schedule(tickets):
                 "ticket": ticket["ticket"],
                 "customer": ticket["customer"],
                 "creation": ensure_datetime(ticket["creation"]),
+                "status": ticket["status"],
                 "hours": float(ticket["hours"]),
             })
 
@@ -131,17 +121,21 @@ def build_schedule(tickets):
     schedule_entries = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
     all_dates = set()
 
+    planning_start = get_today_work_start()
+
     for user, queue in user_queues.items():
-        queue.sort(key=lambda x: (x["creation"], x["ticket"]))
-        current_dt = None
+        queue.sort(
+            key=lambda x: (
+                0 if cstr(x["status"]).lower() == "working" else 1,
+                x["creation"],
+                x["ticket"]
+            )
+        )
+
+        current_dt = planning_start
 
         for task in queue:
             remaining_hours = float(task["hours"])
-
-            if current_dt is None:
-                current_dt = normalize_start_datetime(task["creation"])
-            else:
-                current_dt = max(current_dt, normalize_start_datetime(task["creation"]))
 
             while remaining_hours > 0:
                 current_dt = move_to_working_time(current_dt)
@@ -156,8 +150,6 @@ def build_schedule(tickets):
                 allocated = min(remaining_hours, available_today)
 
                 date_key = current_dt.date().strftime("%Y-%m-%d")
-
-                # show ticket no in date column
                 schedule_entries[task["customer"]][user][date_key].append(task["ticket"])
                 all_dates.add(date_key)
 
@@ -173,18 +165,20 @@ def ensure_datetime(value):
     return frappe.utils.get_datetime(value)
 
 
-def normalize_start_datetime(dt):
-    dt = ensure_datetime(dt)
-    day_start = datetime.combine(dt.date(), time(DAY_START_HOUR, DAY_START_MINUTE))
-    day_end = get_day_end(day_start)
+def get_today_work_start():
+    today = frappe.utils.nowdate()
+    return frappe.utils.get_datetime(
+        today + " " + str(DAY_START_HOUR).zfill(2) + ":" + str(DAY_START_MINUTE).zfill(2) + ":00"
+    )
 
-    if dt < day_start:
-        return day_start
 
-    if dt >= day_end:
-        return next_workday_start(dt)
+def add_hours(dt, hours):
+    return ensure_datetime(dt) + timedelta(hours=hours)
 
-    return dt
+
+def get_day_end(dt):
+    day_start = datetime.combine(ensure_datetime(dt).date(), time(DAY_START_HOUR, DAY_START_MINUTE))
+    return add_hours(day_start, WORKING_HOURS_PER_DAY)
 
 
 def move_to_working_time(dt):
@@ -206,18 +200,9 @@ def next_workday_start(dt):
     return datetime.combine(next_day, time(DAY_START_HOUR, DAY_START_MINUTE))
 
 
-def get_day_end(dt):
-    day_start = datetime.combine(ensure_datetime(dt).date(), time(DAY_START_HOUR, DAY_START_MINUTE))
-    return add_hours(day_start, WORKING_HOURS_PER_DAY)
-
-
 def hours_between(start_dt, end_dt):
     seconds = (end_dt - start_dt).total_seconds()
     return max(seconds / 3600.0, 0)
-
-
-def add_hours(dt, hours):
-    return ensure_datetime(dt) + timedelta(hours=hours)
 
 
 def scrub_fieldname(value):
@@ -226,7 +211,6 @@ def scrub_fieldname(value):
 
 def get_columns(all_dates):
     columns = [
-        # helper fields for tree/collapse
         {
             "label": "Row ID",
             "fieldname": "row_id",
@@ -272,12 +256,21 @@ def get_columns(all_dates):
     return columns
 
 
-def get_data(schedule_entries, all_dates):
+def get_data(schedule_entries, all_dates, filters):
+    view_by = filters.get("view_by") or "Customer"
+
+    if view_by == "User":
+        return get_user_wise_data(schedule_entries, all_dates)
+
+    return get_customer_wise_data(schedule_entries, all_dates)
+
+
+def get_customer_wise_data(schedule_entries, all_dates):
     data = []
 
     for customer in sorted(schedule_entries.keys()):
-        # parent row = customer
         parent_row_id = f"CUST::{customer}"
+
         parent_row = {
             "row_id": parent_row_id,
             "parent_row": "",
@@ -286,7 +279,6 @@ def get_data(schedule_entries, all_dates):
             "user": "",
         }
 
-        # optional summary on parent row
         for date_str in all_dates:
             fieldname = scrub_fieldname(date_str)
             summary_tickets = []
@@ -296,7 +288,6 @@ def get_data(schedule_entries, all_dates):
 
         data.append(parent_row)
 
-        # child rows = users under customer
         for user in sorted(schedule_entries[customer].keys()):
             child_row = {
                 "row_id": f"{parent_row_id}::USER::{user}",
@@ -309,6 +300,55 @@ def get_data(schedule_entries, all_dates):
             for date_str in all_dates:
                 fieldname = scrub_fieldname(date_str)
                 tickets = schedule_entries[customer][user].get(date_str, [])
+                child_row[fieldname] = ", ".join(tickets)
+
+            data.append(child_row)
+
+    return data
+
+
+def get_user_wise_data(schedule_entries, all_dates):
+    data = []
+
+    user_map = defaultdict(lambda: defaultdict(lambda: defaultdict(list)))
+
+    for customer in schedule_entries:
+        for user in schedule_entries[customer]:
+            for date_str in schedule_entries[customer][user]:
+                user_map[user][customer][date_str].extend(schedule_entries[customer][user][date_str])
+
+    for user in sorted(user_map.keys()):
+        parent_row_id = f"USER::{user}"
+
+        parent_row = {
+            "row_id": parent_row_id,
+            "parent_row": "",
+            "indent": 0,
+            "customer": "",
+            "user": user,
+        }
+
+        for date_str in all_dates:
+            fieldname = scrub_fieldname(date_str)
+            summary_tickets = []
+            for customer in sorted(user_map[user].keys()):
+                summary_tickets.extend(user_map[user][customer].get(date_str, []))
+            parent_row[fieldname] = ", ".join(summary_tickets)
+
+        data.append(parent_row)
+
+        for customer in sorted(user_map[user].keys()):
+            child_row = {
+                "row_id": f"{parent_row_id}::CUST::{customer}",
+                "parent_row": parent_row_id,
+                "indent": 1,
+                "customer": customer,
+                "user": "",
+            }
+
+            for date_str in all_dates:
+                fieldname = scrub_fieldname(date_str)
+                tickets = user_map[user][customer].get(date_str, [])
                 child_row[fieldname] = ", ".join(tickets)
 
             data.append(child_row)
